08ec7d79b7787f8dd43bb01155e19826b9ab32d7
Fadil Sutomo <fsutomo@gmail.com>
2016-07-29 05:33:25 +0700

    Relay with remote GraphQL works

--- index.android.js
+++ index.android.js
@@ -1,20 +1,100 @@
 /**
- * This file provided by Facebook is for non-commercial testing and evaluation
- * purposes only.  Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
  */
 
-'use strict';
+ import Relay, {
+   Route,
+   RootContainer,
+   DefaultNetworkLayer
+ } from 'react-relay'
 
+Relay.injectNetworkLayer(new DefaultNetworkLayer('http://localhost:5000/graphql'))
+
+import React, { Component } from 'react';
 import {
   AppRegistry,
+  StyleSheet,
+  Text,
+  View
 } from 'react-native';
-import TodoMVC from './app';
 
-AppRegistry.registerComponent('TodoMVC', () => TodoMVC);
+class StapleNextAndroid extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <RootContainer
+          Component={PersonInfo}
+          route={new PersonRoute({personID: '1'})}
+          renderFetched={(data) => <PersonInfo {...this.props} {...data} />}
+        />
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+class PersonRoute extends Route {
+  static paramDefinitions = {
+    personID: { required: true }
+  }
+  static queries = {
+    person: () => Relay.QL`
+      query {
+        person(id: $personID)
+      }
+    `
+  }
+  static routeName = 'PersonRoute'
+}
+
+class PersonInfo extends Component {
+  render () {
+    const person = this.props.person
+    return (
+      <Text>email: {person.email}</Text>
+    )
+  }
+}
+
+PersonInfo = Relay.createContainer(PersonInfo, {
+  fragments: {
+    person: () => Relay.QL`
+      fragment on Person {
+        id,
+        email
+      }
+    `
+  }
+})
+
+AppRegistry.registerComponent('StapleNextAndroid', () => StapleNextAndroid);
